791df43ad44d6dca9a9fc158eb733c5d588b999e
madhumitanawkar05-crypto <madhumitanawkar05@gmail.com>
2026-03-08 18:10:06 +0530

    Add files via upload

---
+++ VariablesDataTypes.java
@@ -0,0 +1,18 @@
+public class VariablesDataTypes {
+    public static void main(String[] args) {
+        
+        int age = 21; 
+        double salary = 55000.50; 
+        char grade = 'A'; 
+        String name = "Madhumita"; 
+        boolean isEmployed = true; 
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: $" + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Employed: " + isEmployed);
+}
+
+    
+}
