a82a858a875f46dc4c00b4be96ab165b4bd7acfd
madhumitanawkar05-crypto <madhumitanawkar05@gmail.com>
2026-03-15 23:59:04 +0530

    Add files via upload

---
+++ SquareNumber.java
@@ -0,0 +1,8 @@
+public class SquareNumber{
+    public static void main(String arg[]){
+        for(int i=1; i<=5;i++){
+            int square = i * i;
+            System.out.println(" Square of " + i + " = " + square);
+        }
+    }
+}
